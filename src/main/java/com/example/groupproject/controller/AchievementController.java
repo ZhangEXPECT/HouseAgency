36f4beb33e1f1077d09bad8312c900746ea6e04f
2019211001000916GanZhou <2592720526@qq.com>
2022-06-22 18:36:17 +0800

    ganzhou 2022-06-22 绩效管理 commit

--- src/main/java/com/example/groupproject/controller/AchievementController.java
+++ src/main/java/com/example/groupproject/controller/AchievementController.java
@@ -6,6 +6,8 @@ import com.example.groupproject.service.AchievementService;
 import com.example.groupproject.utils.Achievement;
 import com.example.groupproject.utils.Result;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,18 +30,29 @@ public class AchievementController {
 
     @GetMapping("/getOrderByEmpId")
     @ApiOperation("获取员工达成的订单")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "empId", value = "客户Id", dataType = "int"),
+    })
     public Result getMyManageOrder(Integer empId){
         List<Order> list = this.achievementService.getMyManageOrder(empId);
         return new Result(200,"成功",list);
     }
     @GetMapping("/getMyAchievement")
-    @ApiOperation("获取员工的总提成")
+    @ApiOperation("获取员工的提成")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "empId", value = "客户Id", dataType = "int"),
+            @ApiImplicitParam(name = "quarter", value = "季度(0:全部;1:一季度;2:二季度;3:三季度;4:四季度;)", dataType = "int"),
+    })
     public Result getMyAchievement(Integer empId,Integer quarter){
         Achievement achievement = this.achievementService.getMyAchievement(empId,quarter);
         return new Result(200,"成功",achievement);
     }
     @GetMapping("/achievementRankList")
     @ApiOperation("员工排行榜")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "type", value = "榜单类型(1:提成金额;2:订单数量;)", dataType = "int"),
+            @ApiImplicitParam(name = "quarter", value = "季度(0:全部;1:一季度;2:二季度;3:三季度;4:四季度;)", dataType = "int"),
+    })
     public Result achievementRankList(Integer type,Integer quarter){
         Stream<Achievement> list = this.achievementService.achievementRankList(type,quarter);
         return new Result(200,"成功",list);
